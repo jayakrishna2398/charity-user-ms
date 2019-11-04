8630b12bf7bae3030cd4210ac749406e7cb3116d
krishnamoorthy <krishna192168@gmail.com>
2019-11-04 18:01:21 +0530

    change profile dev to prod

--- src/main/java/com/charityapp/userappms/config/CorsConfig.java
+++ src/main/java/com/charityapp/userappms/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.charityapp.userappms.config;
+	package com.charityapp.userappms.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
